656a34e13c4b2ac3c3fb375cf0a522c6d2fda0f6
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-05-10 09:47:52 -0500

    styles

--- src/pages/use.tsx
+++ src/pages/use.tsx
@@ -1,8 +1,12 @@
+/**
+ * Contains a hosted version of the World ID JS integration/widget, ready to be used in staging & production
+ */
 import React, { useEffect } from "react";
 import Head from "@docusaurus/Head";
 import styles from "./use.module.scss";
 import Logo from "@site/static/img/logo.svg";
 import WorldID from "@worldcoin/id";
+import Link from "@docusaurus/Link";
 
 const PAGE_DESCRIPTION =
   "Prove you're a unique human without revealing any personal data";
@@ -31,6 +35,15 @@ export default function HostedWorldID(): JSX.Element {
             Verify you are a unique human with World ID
           </p>
           <div id="world-id-container" />
+          <div className={styles.btnContainer}>
+            <button className={styles.btnContinue} disabled>
+              Continue
+            </button>
+          </div>
+        </div>
+        <div className={styles.learnMore}>
+          Learn more about <Link to="/docs">World ID</Link> and about{" "}
+          <Link to="https://worldcoin.org">Worldcoin</Link>.
         </div>
       </div>
     </>

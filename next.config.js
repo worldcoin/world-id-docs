02df1732dcab85885b5de56dd549e7d7c997b6da
Denis Bushaev <denis.bushaev@ottofeller.com>
2022-09-28 18:35:49 +0300

    add redirects

--- next.config.js
+++ next.config.js
@@ -14,6 +14,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: '/docs/:slug*',
+        destination: '/:slug*', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = withMDX(nextConfig);

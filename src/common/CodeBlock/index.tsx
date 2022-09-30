48fdbeb2d4c0d7101b79e8cd81b099ec5b7220b0
Igor Osipov <igor.osipov@ottofeller.com>
2022-09-30 19:28:15 +0300

    fix: remove backticks from all the code tags

--- src/common/CodeBlock/index.tsx
+++ src/common/CodeBlock/index.tsx
@@ -1,11 +1,6 @@
 import cn from 'classnames'
 import Highlight, { Language, defaultProps } from 'prism-react-renderer'
-import {
-  Fragment,
-  memo,
-  ReactNode,
-  useMemo,
-} from 'react'
+import { Fragment, memo, ReactNode, useMemo } from 'react'
 
 export const CodeBlock = memo(function CodeBlock(props: {
   children?: ReactNode
@@ -34,7 +29,10 @@ export const CodeBlock = memo(function CodeBlock(props: {
     >
       {({ className, style, tokens, getTokenProps }) => (
         <code
-          className={cn('relative !font-roboto-mono', className)}
+          className={cn(
+            'relative !font-roboto-mono before:!content-none after:!content-none',
+            className
+          )}
           style={style}
         >
           {props.showLines && <span className="line-border" />}
@@ -50,12 +48,14 @@ export const CodeBlock = memo(function CodeBlock(props: {
               )}
 
               {line
-                .filter((token) => !token.empty)
+                .filter((token) => {
+                  return !token.empty
+                })
                 .map((token, tokenIndex) => (
                   <span key={tokenIndex} {...getTokenProps({ token })} />
                 ))}
 
-              {'\n'}
+              {tokens.length > 1 && '\n'}
             </Fragment>
           ))}
         </code>

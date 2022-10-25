5b56484ac71cb9df6d6a21173b6509816e07ee2a
Denis Bushaev <denis.bushaev@ottofeller.com>
2022-10-25 12:57:29 +0300

    Implement new admonitions (#76)

--- src/common/Callout/index.tsx
+++ src/common/Callout/index.tsx
@@ -1,53 +1,68 @@
 import cn from 'classnames'
-import { Icon } from 'common/Icon'
+import { Icon, IconType } from 'common/Icon'
 import { memo, ReactNode, useMemo } from 'react'
 import { styles } from 'common/helpers/styles'
 
 export const Callout = memo(function Callout(props: {
-  type?: 'note' | 'warning'
-  title: string
+  type?: 'note' | 'tip' | 'info' | 'caution' | 'danger'
+  title?: string
   children: ReactNode
 }) {
   const type = useMemo(() => props.type ?? 'note', [props.type])
 
-  const titleStyle = useMemo(
-    () =>
-      cn({
-        'bg-8e87ff dark:bg-gradient-to-r from-fff0ed to-edecfc':
-          type === 'note',
-        'bg-ff6848': type === 'warning',
-      }),
-    [type]
-  )
+  const title = useMemo(() => {
+    if (props.title) {
+      return props.title
+    }
+
+    return `${type?.slice(0, 1).toUpperCase()}${type?.slice(1)}`
+  }, [props.title, type])
 
   return (
-    <div className="relative my-6">
+    <div
+      className={cn('relative my-6', {
+        '[--color:_#596673] dark:[--color:_#9eafc0] [--link-color:_#64b483]': type === 'note',
+        '[--color:_#64b483]': type === 'tip',
+        '[--color:_#8e87ff]': type === 'info',
+        '[--color:_#ff6848]': type === 'caution',
+        '[--color:_#ff5a76]': type === 'danger',
+      })}
+    >
       <div className={styles.fenceBorder} />
 
       <div
         className={cn(
-          'grid gap-2 rounded-2xl p-6 dark:bg-161b22',
+          'grid gap-2 rounded-2xl p-6',
+          {
+            'bg-[#edeeee] dark:bg-181b1f': type === 'note',
+            'bg-[#dfeae3] dark:bg-[#325142]': type === 'tip',
+            'bg-[#e5e4f6] dark:bg-[#39385f]': type === 'info',
+            'bg-[#f6dfda] dark:bg-[#5b2f28]': type === 'caution',
+            'bg-[#f6dde1] dark:bg-[#5b2b36]': type === 'danger',
+          },
           styles.fenceBorderInner
         )}
       >
-        <div className="flex items-center gap-x-2.5">
-          <Icon
-            name="question-circle"
-            className={cn('h-5 w-5', titleStyle)}
-            noMask
-          />
+        <div className="border-l-[3px] border-[color:var(--color)] pl-6">
+          <div className="flex items-center gap-x-2.5">
+            <Icon
+              name={
+                {
+                  note: 'info',
+                  tip: 'speaker',
+                  info: 'question',
+                  caution: 'attention',
+                  danger: 'lightning',
+                }[type] as IconType
+              }
+              className="h-5 w-5 !bg-[color:_var(--color)]"
+              noMask
+            />
+            <p className={cn('m-0 text-20 font-semibold ![color:_var(--color)]')}>{title}</p>
+          </div>
 
-          <p
-            className={cn(
-              'm-0 bg-clip-text text-20 font-semibold !text-transparent',
-              titleStyle
-            )}
-          >
-            {props.title}
-          </p>
+          <div className="[&>*]:mb-0 [&_*]:inline [&_a]:[color:_var(--link-color,_var(--color))]">{props.children}</div>
         </div>
-
-        <div className="">{props.children}</div>
       </div>
     </div>
   )

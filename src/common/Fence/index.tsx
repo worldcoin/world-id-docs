da150ec5e22e2e73ddbb658d25ac6789592d4d5d
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-02-27 18:18:35 +0400

    feat: codeblock heading

--- src/common/Fence/index.tsx
+++ src/common/Fence/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 export function Fence(props: { children?: ReactNode; className?: string }) {
   return (
-    <div className={cn('relative border border-neutral-200 dark:border-neutral-500 rounded-lg', props.className)}>
+    <div className={cn('relative border border-neutral-200 dark:border-neutral-500 rounded-lg pre', props.className)}>
       <pre
         className={cn(
           'max-w-[calc(100vw_-_32px)] m-0 overflow-auto',

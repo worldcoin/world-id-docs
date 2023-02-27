90ea79f9e3d34e542303be8918b6290b1cfe439d
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-02-27 18:39:18 +0400

    fix: table text color

--- src/Layout/Prose/index.tsx
+++ src/Layout/Prose/index.tsx
@@ -37,6 +37,7 @@ export function Prose({
         'prose-table:border-separate prose-table:border-spacing-0 prose-table:border prose-table:border-neutral-100 dark:prose-table:border-neutral-700 prose-table:rounded-lg',
         'prose-th:leading-5 prose-th:px-6 prose-th:py-4',
         'prose-td:px-6 prose-td:py-4 prose-td:border-t prose-td:border-neutral-100 dark:prose-td:border-neutral-700',
+        'prose-td:text-neutral-500'
       )}
     >
       {children}

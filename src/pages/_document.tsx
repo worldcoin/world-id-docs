459a540affa0b1a27ea2aba94c89447cf557903a
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-04 12:05:12 -0600

    remove legacy JS package

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
+      <Head />
       <body className="bg-neutral-0 font-default text-neutral-900 dark:bg-neutral-900 dark:text-neutral-0">
         <Main />
         <NextScript />
---
+++ src/pages/try.tsx
@@ -0,0 +1,9 @@
+const Try = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Try It Out</h1>
+    </div>
+  )
+}
+
+export default Try

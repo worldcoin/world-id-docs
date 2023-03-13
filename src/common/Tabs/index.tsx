9b5597af477862a6da4dc7198788bf7996c0fba7
Max Petretta <git@maxpetretta.com>
2023-03-13 09:44:39 +0100

    feat: add tab components to simulator page (#114)
    Co-authored-by: Paolo D'Amico <paolodamico@users.noreply.github.com>

---
+++ src/common/Tabs/index.tsx
@@ -0,0 +1,41 @@
+import React, {
+  memo,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+  useState,
+} from 'react'
+import { Tab } from './Tab'
+
+interface TabItemProps {
+  label: string
+}
+
+export const Tabs = memo(function Tabs(props: { children: ReactNode }) {
+  const [currentTab, setCurrentTab] = useState(0)
+
+  return (
+    <div className="p-2">
+      {React.Children.map(
+        props.children as ReactElement<PropsWithChildren<TabItemProps>>,
+        (child, index) => {
+          return (
+            child && (
+              <Tab
+                key={index}
+                label={child.props.label}
+                isActive={currentTab === index}
+                onSelect={() => setCurrentTab(index)}
+              />
+            )
+          )
+        }
+      )}
+      {React.Children.map(props.children, (child, index) => {
+        return React.cloneElement(child as React.ReactElement<any>, {
+          isActive: currentTab === index,
+        })
+      })}
+    </div>
+  )
+})

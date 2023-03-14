6520c34daa4daf76f79e8830620bc4769df1245d
igorosip0v <igor.osipov@ottofeller.com>
2023-03-14 11:22:20 +0100

    fix: update list of pages without layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -56,7 +56,7 @@ export default function MyApp(pageProps: AppProps) {
   const pageDescription = findPageDescription(pageHtml)
 
   const isDefaultLayoutPage = useMemo(
-    () => !['/use'].includes(router.pathname),
+    () => !([] as Array<string>).includes(router.pathname),
     [router.pathname]
   )
 

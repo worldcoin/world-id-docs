d29fa2f14506f4e4c8d401ae59ce970f9ec12ae2
igorosip0v <89008845+igorosip0v@users.noreply.github.com>
2023-03-14 16:58:46 +0100

    feat: add favicon (#144)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,12 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 			<Head>
 				{router.pathname === '/' ? <title>World ID</title> : <title>{`${pageProps.title} - World ID`}</title>}
 				<meta name="description" content={pageProps.description} />
+
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#191919" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
 			</Head>
 			{process.env.NODE_ENV === 'production' && <Clippy theme="light" />}
 			{/* @ts-ignore */}

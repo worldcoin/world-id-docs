6657e0da4dbaf57e03074204df52b16ca43413bb
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-14 14:21:00 +0400

    feat: new tabs (#132)
    Co-authored-by: Paolo D'Amico <paolodamico@users.noreply.github.com>

--- src/common/Tabs/index.tsx
+++ src/common/Tabs/index.tsx
@@ -1,8 +1,12 @@
+import cn from 'classnames'
 import React, {
   memo,
   PropsWithChildren,
   ReactElement,
   ReactNode,
+  useEffect,
+  useMemo,
+  useRef,
   useState,
 } from 'react'
 import { Tab } from './Tab'
@@ -12,28 +16,58 @@ interface TabItemProps {
 }
 
 export const Tabs = memo(function Tabs(props: { children: ReactNode }) {
+  const tabsRef = useRef<HTMLDivElement>(null)
   const [currentTab, setCurrentTab] = useState(0)
+
+  const tabs = useMemo(() => {
+    return React.Children.map(
+      props.children as ReactElement<PropsWithChildren<TabItemProps>>,
+      (child) => (child.props?.label ? child : null)
+    ).filter((child) => child !== null)
+  }, [props.children])
+
+  // NOTE: indicator set position
+  useEffect(() => {
+    const Tabs = tabsRef.current
+
+    if (!Tabs) {
+      return
+    }
+
+    const CurrentTab = Tabs.childNodes[currentTab] as HTMLDivElement
+
+    if (!CurrentTab) {
+      return
+    }
+
+    Tabs.style.setProperty('--indicator-left', `${CurrentTab.offsetLeft}px`)
+    Tabs.style.setProperty('--indicator-width', `${CurrentTab.clientWidth}px`)
+  }, [currentTab])
 
   return (
     <div className="p-2">
-      <div className="flex flex-col md:flex-row">
-        {React.Children.map(
-          props.children as ReactElement<PropsWithChildren<TabItemProps>>,
-          (child, index) => {
-            return (
-              child && (
-                <Tab
-                  key={index}
-                  label={child.props.label}
-                  isActive={currentTab === index}
-                  onSelect={() => setCurrentTab(index)}
-                />
-              )
-            )
-          }
+      <div
+        ref={tabsRef}
+        className={cn(
+          'relative flex flex-col gap-10 border-b border-f3f4f5 md:flex-row',
+          'after:absolute after:bottom-0 after:left-[var(--indicator-left)] after:h-px after:w-[var(--indicator-width)] after:bg-191c20 after:transition-all'
         )}
+      >
+        {tabs.map((tab, index) => {
+          return (
+            tab.props && (
+              <Tab
+                key={index}
+                label={tab.props.label}
+                isActive={currentTab === index}
+                onSelect={() => setCurrentTab(index)}
+              />
+            )
+          )
+        })}
       </div>
-      {React.Children.map(props.children, (child, index) => {
+
+      {tabs.map((child, index) => {
         return React.cloneElement(child as React.ReactElement<any>, {
           isActive: currentTab === index,
         })
@@ -41,3 +75,5 @@ export const Tabs = memo(function Tabs(props: { children: ReactNode }) {
     </div>
   )
 })
+
+export { TabItem } from './TabItem'

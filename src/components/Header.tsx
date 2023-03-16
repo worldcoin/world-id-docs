d40b68a0a1c8041418c2871fb31b35b2c11ea8ca
igorosip0v <igor.osipov@ottofeller.com>
2023-03-16 13:46:58 +0100

    fix: make external links opens in a new tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
-import Link from 'next/link'
 import { Button } from './Button'
 import Logo from '@/components/Logo'
+import { Link } from '@/components/Link'
 import GitHubIcon from './icons/GitHubIcon'
 import { MobileSearch, Search } from './Search'
 import { useMobileNavigationStore } from '@/components/MobileNavigation'
@@ -79,7 +79,7 @@ export const Header: ForwardRefExoticComponent<{ className?: string }> = forward
 					<ul role="list" className="flex items-center gap-4">
 						<TopLevelNavItem
 							className="bg-gray-100 hover:bg-gray-100/50 py-[7px] px-4 rounded-lg border border-gray-200"
-							href="https://docs.worldcoin.org/waitlist"
+							href="https://toolsforhumanity.typeform.com/sdk-waitlist"
 							target="_blank"
 						>
 							Join the Waitlist
--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import Link from 'next/link'
+import { Link } from '@/components/Link'
 import { Heading } from '@/components/Heading'
 import { FC, Fragment, PropsWithChildren, ReactNode } from 'react'
 export { Button } from '@/components/Button'

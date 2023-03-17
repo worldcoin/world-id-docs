232d587a3501512643b98b91baae972ce54e3706
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-17 12:05:45 +0100

    fix: sitemap (#151)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,14 @@
 module.exports = {
 	siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
 	generateRobotsTxt: true,
+	transform: async (config, path) => {
+		if (path.includes('/api-docs')) {
+			return {
+				...config,
+				loc: path.replace('/api-docs', '/api'),
+			}
+		}
+
+		return config
+	},
 }

03c6c2d1a54a9d21037a3bc7edfa63a8f572510b
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-17 13:48:19 +0100

    fix: this time for real (#154)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,11 +5,20 @@ module.exports = {
 	transform: async (config, path) => {
 		if (path.includes('/api-docs')) {
 			return {
-				...config,
 				loc: path.replace('/api-docs', '/api'),
+				changefreq: config.changefreq,
+				priority: config.priority,
+				lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+				alternateRefs: config.alternateRefs ?? [],
 			}
 		}
-
-		return config
+		// Use default transformation for all other cases
+		return {
+			loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+			changefreq: config.changefreq,
+			priority: config.priority,
+			lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+			alternateRefs: config.alternateRefs ?? [],
+		}
 	},
 }

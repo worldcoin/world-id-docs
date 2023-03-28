93fcc45058acf398eef7f70d17247f8a6ff29ceb
igorosip0v <89008845+igorosip0v@users.noreply.github.com>
2023-03-28 16:18:24 +0200

    feat: cookie banner (#157)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	plugins: {
+		'postcss-import': {},
+    'tailwindcss/nesting': {},
+    'postcss-selector-matches': {},
 		tailwindcss: {},
 		'postcss-focus-visible': {
 			replaceWith: '[data-focus-visible-added]',
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="bg-white antialiased dark:bg-zinc-900">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<script
+					src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+					type="text/javascript"
+					data-domain-script={process.env.NEXT_PUBLIC_COOKIEPRO_DOMAIN_SCRIPT}
+					async
+				/>
+			</Head>
+
+			<body className="bg-white antialiased dark:bg-zinc-900">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }

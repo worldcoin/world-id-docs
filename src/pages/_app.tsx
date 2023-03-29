19765281ad2e01c2a1447d73152b49117521e7ef
Mickh F <michael.furutin@ottofeller.com>
2023-03-29 20:13:25 +0300

    chore: add posthog key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ Router.events.on('hashChangeStart', onRouteChange)
 
 if (typeof window !== 'undefined') {
 	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+		api_host: 'https://app.posthog.com',
 		// Disable in development
 		loaded: (posthog) => {
 			if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()

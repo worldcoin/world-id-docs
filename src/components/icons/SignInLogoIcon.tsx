20b9263e292a62bca9a241a4f555fafb9e280e9b
igorosip0v <igor.osipov@ottofeller.com>
2023-04-17 09:29:48 +0200

    refactor: apps page card fixes

---
+++ src/components/icons/SignInLogoIcon.tsx
@@ -0,0 +1,46 @@
+import { FC, SVGAttributes } from 'react'
+
+const SignInLogoIcon: FC<SVGAttributes<SVGElement>> = props => (
+	<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<g clipPath="url(#clip0_20199_55312)">
+			<path
+				d="M12.562 8.41602L10.5163 10.4616C10.1356 10.8422 9.51521 10.8422 9.13457 10.4616L7.08887 8.41769"
+				stroke="currentColor"
+				strokeWidth="1.30608"
+				strokeMiterlimit="10"
+				strokeLinecap="round"
+			/>
+			<path
+				d="M9.82471 12.6457V11.6078C9.82471 11.3178 9.94041 11.0394 10.145 10.8349L12.5445 8.43555"
+				stroke="currentColor"
+				strokeWidth="1.30608"
+				strokeMiterlimit="10"
+				strokeLinecap="round"
+			/>
+			<path
+				d="M9.80914 12.6415V11.6036C9.80914 11.3136 9.69344 11.0352 9.48887 10.8307L7.08936 8.42969"
+				stroke="currentColor"
+				strokeWidth="1.30608"
+				strokeMiterlimit="10"
+				strokeLinecap="round"
+			/>
+			<path
+				d="M9.82653 8.51811C10.4766 8.51811 11.0037 7.99114 11.0037 7.34109C11.0037 6.69104 10.4766 6.16406 9.82653 6.16406C9.17643 6.16406 8.64941 6.69104 8.64941 7.34109C8.64941 7.99114 9.17643 8.51811 9.82653 8.51811Z"
+				fill="#191C20"
+			/>
+		</g>
+		<path
+			d="M6.98565 3.32617H5.56595C4.31143 3.32617 3.29443 4.3431 3.29443 5.59754V7.01714M12.6644 16.3865H14.0841C15.3387 16.3865 16.3557 15.3696 16.3557 14.1151V12.6955M16.3557 7.01714V5.59754C16.3557 4.3431 15.3387 3.32617 14.0841 3.32617L12.6644 3.32617M3.29443 12.6955L3.29443 14.1151C3.29443 15.3696 4.31143 16.3865 5.56595 16.3865H6.98565"
+			stroke="currentColor"
+			strokeWidth="1.30608"
+			strokeLinecap="round"
+		/>
+		<defs>
+			<clipPath id="clip0_20199_55312">
+				<rect width="6.81455" height="7.14934" fill="white" transform="translate(6.41699 6.16406)" />
+			</clipPath>
+		</defs>
+	</svg>
+)
+
+export default SignInLogoIcon

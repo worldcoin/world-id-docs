1b011e9c0755ecb49e74f941ef93a3ea0888d40f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-05-11 14:03:23 -0700

    Update svg.d.ts

--- @types/svg.d.ts
+++ @types/svg.d.ts
@@ -1,1 +1,6 @@
-declare module '*.svg' {}
+declare module '*.svg' {
+	import React from 'react'
+	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+	const src: string
+	export default src
+}

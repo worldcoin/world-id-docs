5c1f7bd89f7af7f5b19e6aacd7b00f955e4bb16a
0xPenryn <penryn@pm.me>
2023-07-19 16:24:16 +0200

    feat: individual page per integration (#198)
    * feat: individual page per integration
    
    still needs content work, but it's in a decent place rn
    
    * feat: visual updates
    
    * feat: further updates
    
    * fix: update "add your app" link
    
    * feat: update descriptions and images
    
    * fix: cspell config
    
    * chore: update worldcoin app images
    
    * feat: add legacy flag for integrations
    
    * feat: add talent protocol
    
    * fix: stale console.log
    
    * fix: weird hover colors
    
    * fix: remove commented code
    
    * fix: tags appearance
    
    * chore: mark poap as legacy

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -1,14 +1,15 @@
 import { Link } from './Link'
 import Image from 'next/image'
 import cn, { clsx } from 'clsx'
-import LogoIcon from './icons/LogoIcon'
+import LogoIcon from '@/components/icons/LogoIcon'
 import { Button } from '@/components/Button'
-import SignInLogoIcon from './icons/SignInLogoIcon'
-import GitHubIcon from '@/components/icons/GitHubIcon'
 import { AllHTMLAttributes, FC, PropsWithChildren, useCallback, useState } from 'react'
-import RedirectIcon from './icons/RedirectIcon'
+import appList from '../pages/apps/appList.json'
+import ArrowIcon from './icons/ArrowIcon'
 
 type Props = PropsWithChildren<{ className?: string } & AllHTMLAttributes<HTMLElement>>
+
+const sortedApps = appList.apps.sort((a, b) => a.title.localeCompare(b.title))
 
 // ANCHOR: Apps component
 export const Apps: FC<Props> = props => {
@@ -44,7 +45,7 @@ export const Apps: FC<Props> = props => {
 		[filter, isTagSelected]
 	)
 
-	const bookmarkedApps = apps.filter(app => app.bookmark)
+	const bookmarkedApps: Array<AppConfig> = sortedApps.filter(app => app.bookmark)
 
 	return (
 		<div className={clsx('relative', props.className)}>
@@ -56,20 +57,27 @@ export const Apps: FC<Props> = props => {
 				height={474}
 			/>
 
-			{/* TODO: update with actual link */}
-			<Link href='https://typeform.com'> 
+			<Link href='https://github.com/worldcoin/world-id-docs/blob/main/src/pages/apps/appList.json'>
 				<Button className="px-6 py-4.5 !font-bold leading-3 uppercase rounded-xl">
 					Add your app
 				</Button>
 			</Link>
 			<div className="pt-4" />
 
-			<h1 className="text-xl font-bold text-zinc-900 dark:text-white mt-5"> Highlights </h1>
+			{(bookmarkedApps.length > 0) && <div>
+				<h1 className="text-xl font-bold text-zinc-900 dark:text-white mt-5"> Highlights </h1>
 
-			<div className="mt-5 grid grid-cols-1 gap-6 lg:grid-cols-2">
-				<Card app={bookmarkedApps[0]} />
-				<Card app={bookmarkedApps[1]} />
-			</div>
+				<div className="mt-5 grid grid-cols-1 gap-6 lg:grid-cols-2">
+					{bookmarkedApps.map((app, id) => {
+						return (
+							<Card
+								key={id}
+								app={app as AppConfig}
+							/>
+						)
+					})}
+				</div>
+			</div>}
 
 			<hr className="my-10" />
 			<h1 className="text-xl font-bold text-zinc-900 dark:text-white mt-5"> All Integrations </h1>
@@ -87,11 +95,12 @@ export const Apps: FC<Props> = props => {
 			</div>
 
 			<div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-				{apps.map((example, id) => {
-					if (filter.length === 0 || example.tags.some(tag => isTagSelected(tag))) {
+				{sortedApps.map((example, id) => {
+					if ((filter.length === 0 || example.tags.some(tag => isTagSelected(tag)))) {
 						return (
 							<Item
 								key={id}
+								slug={example.slug}
 								image={example.image}
 								title={example.title}
 								subtitle={example.subtitle}
@@ -113,31 +122,24 @@ export const Apps: FC<Props> = props => {
 }
 
 // ANCHOR: Card component for the cards on the top
-export const Card: FC<{ app: typeof apps[0] }> = ({ app }) => {
+export const Card: FC<{ app: AppConfig }> = ({ app }) => {
 	return (
-		<div className="rounded-lg shadow-card">
-			<Link href={app.url ?? '#'} className="flex relative aspect-card">
-				<Image className="absolute inset-0 m-0" src={app.image.lg} alt={app.title} fill />
-			</Link>
+		<div className="rounded-lg shadow-card hover:cursor-pointer hover:scale-105 transition-all" onClick={() => location.href = `/apps/${app.slug}`}>
+			<div className="flex relative aspect-card">
+				<Image className="absolute inset-0 m-0" src={app.image.lg!} alt={app.title} fill />
+			</div>
 
 			<div className="flex items-center gap-x-6 px-6 py-7">
-				<div className="grow">
-					<div className="font-bold text-base leading-4">{app.title}</div>
+				<div className="flex flex-col grow">
+					<div className="font-bold text-base leading-4 text-gray-700">{app.title}</div>
 					<div className="mt-1 text-base text-gray-500 leading-4">{app.subtitle}</div>
 				</div>
 
 				<div className="flex gap-x-2">
-					{app.githubUrl && (
-						<Link
-							href={app.githubUrl}
-							className="flex items-center justify-center w-9 h-9 bg-gray-100 hover:bg-gray-200/70 transition-colors rounded-full"
-						>
-							<GitHubIcon className="w-5 h-5" />
-						</Link>
-					)}
-
-					<div className="flex items-center justify-center w-9 h-9 bg-gray-900 hover:bg-gray-900/80 transition-colors rounded-full">
-						<RedirectIcon className="text-gray-100" />
+					<div
+						className="flex items-center justify-center w-9 h-9 bg-gray-900 hover:bg-gray-900/80 transition-colors rounded-full"
+					>
+						<ArrowIcon className="text-gray-100 h-" />
 					</div>
 				</div>
 			</div>
@@ -162,6 +164,7 @@ export const Tag = (props: { selected: boolean; onClick: () => void; children: s
 
 // ANCHOR: Card component for the list of the apps
 export const Item = (props: {
+	slug: string
 	image: {
 		sm: string
 		lg?: string
@@ -176,72 +179,28 @@ export const Item = (props: {
 	worldcoin?: boolean
 	onToggleFilter: (tag: string) => void
 }) => {
+
 	return (
-		<div className="relative p-6 border border-gray-100 rounded-lg">
-			{props.bookmark && (
-				<div className="absolute top-0 right-[50px]">
-					<svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path
-							d="M0 0H12V14.0557C12 14.9591 11.0592 15.5546 10.2426 15.1681L6.70526 13.4936C6.25883 13.2823 5.74117 13.2823 5.29474 13.4936L1.75736 15.1681C0.940837 15.5546 0 14.9591 0 14.0557V0Z"
-							fill="#4940E0"
-						/>
-					</svg>
+		<div className="relative px-6 pt-6 pb-4 border border-gray-200 rounded-lg hover:cursor-pointer hover:scale-105 transition-all" onClick={() => location.href = `/apps/${props.slug}`}>
+			<div className="flex flex-row gap-x-4">
+				<div className="relative shrink-0 !h-[80px] !w-[80px]">
+					<Image className="m-0" src={props.image.sm} alt={props.title} fill objectFit='cover' />
 				</div>
-			)}
-			<div className="flex gap-x-4">
-				<div className="relative h-[80px] w-[80px]">
-					<Image className="m-0" src={props.image.sm} alt={props.title} layout="fill" objectFit="cover" />
-				</div>
-				<div className="relative grow flex flex-col">
-					{props.worldcoin && (
-						<div className="absolute top-0 right-0">
-							<LogoIcon className="text-primary" />
-						</div>
-					)}
-					<div className="flex flex-col gap-y-1.5">
-						<div className="font-bold text-base leading-4">{props.title}</div>
-						<div className="text-14 text-gray-500 leading-3">{props.subtitle}</div>
-					</div>
-					<div className="grow flex items-end gap-x-2">
-						{props.githubUrl && (
-						<Button
-							variant="neutral"
-							className="items-center h-8 px-4 gap-x-1 no-underline !rounded-lg "
-							href={props.githubUrl}
-						>
-							<GitHubIcon className="w-4 h-4" />
-							<div className="font-medium text-14 tracking-[-0.01em]">GITHUB</div>
-						</Button>
+				<div className="flex flex-col items-start w-full">
+					<div className="relative align-top items-start justify-between w-full flex flex-row">
+						<div className="font-bold text-base text-gray-700 shrink">{props.title}</div>
+						{props.worldcoin && (
+							<div className="hover:visible shrink" title="Official Integration">
+								<LogoIcon className="text-primary" />
+							</div>
 						)}
-						<Button
-							variant="primary"
-							className="items-center h-8 px-4 gap-x-1 no-underline !rounded-lg"
-							href={props.url}
-						>
-							<div className="font-medium text-14 tracking-[-0.01em]">VISIT</div>
-							<RedirectIcon className="text-gray-100" />
-							{/* <svg
-								width="12"
-								height="12"
-								viewBox="0 0 12 12"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fillRule="evenodd"
-									clipRule="evenodd"
-									d="M4.37068 2.57227L9.05067 2.57227C9.37376 2.57227 9.63567 2.83418 9.63567 3.15727L9.63568 7.83726C9.63568 8.16035 9.37377 8.42226 9.05068 8.42226C8.72759 8.42226 8.46568 8.16035 8.46568 7.83726L8.46568 4.56958L2.71295 10.1127C2.48449 10.3412 2.11409 10.3412 1.88564 10.1127C1.65718 9.88429 1.65718 9.51389 1.88564 9.28543L7.63836 3.74227L4.37068 3.74227C4.04759 3.74227 3.78568 3.48035 3.78568 3.15727C3.78568 2.83418 4.04759 2.57227 4.37068 2.57227Z"
-									fill="#191C20"
-								/>
-							</svg> */}
-						</Button>
 					</div>
+					<div className="text-sm text-gray-500 leading-4 line-clamp-3 max-w-sm">{props.description}</div>
 				</div>
 			</div>
-			<div className="mt-4 text-14 text-gray-500 leading-5 line-clamp-2">{props.description}</div>
-			<div className="mt-4 flex flex-wrap gap-x-3 gap-y-1 text-14 text-primary leading-5 cursor-pointer">
+			<div className="mt-4 flex flex-wrap gap-x-3 gap-y-1 text-14 text-gray-500 leading-5">
 				{props.tags.map((tag, i) => (
-					<div key={i} onClick={() => props.onToggleFilter(tag)}>
+					<div key={i}>
 						{tag}
 					</div>
 				))}
@@ -250,111 +209,8 @@ export const Item = (props: {
 	)
 }
 
-// ANCHOR: List of the apps data
-const apps = [
-	{
-		url: 'https://worldcoin.org/auth0',
-		githubUrl: 'https://github.com/0xPenryn/auth0-world-id-social-connection',
-		image: {
-			sm: '/images/apps/auth0.svg',
-			lg: '/images/apps/auth0-lg.png',
-		},
-		title: 'Auth0',
-		subtitle: 'marketplace.auth0.com',
-		description:
-			'Add Sign In with Worldcoin to your Auth0 tenant in under 5 minutes. No code required.',
-		tags: ['Integration', 'Social', 'Sign In'],
-		bookmark: true,
-	},
-
-	{
-		url: 'https://discordbouncer.com',
-		githubUrl: 'https://github.com/worldcoin/discord-bouncer',
-		image: {
-			sm: '/images/apps/discord.svg',
-			lg: '/images/apps/discord-lg.svg',
-		},
-		title: 'Discord Bouncer',
-		subtitle: 'discordbouncer.com',
-		description: 'Prevent spam and increase the quality of the community by verifying humans.',
-		tags: ['App', 'Integration', 'API'],
-		bookmark: true,
-	},
-
-	{
-		url: 'https://human.withlens.app',
-		githubUrl: 'https://github.com/worldcoin/world-id-lens',
-		image: {
-			sm: '/images/apps/lens.svg',
-			lg: '/images/apps/lens-lg.svg',
-		},
-		title: 'Lens',
-		subtitle: 'human.withlens.app',
-		description:
-			'The decentralized social network. Verify a Lens profile belongs to real person. No bots, reduce spam.',
-		tags: ['On-chain', 'Integration', 'Social'],
-		bookmark: false,
-	},
-
-	{
-		url: 'https://poap.worldcoin.org',
-		githubUrl: 'https://github.com/worldcoin/world-id-poap',
-		image: {
-			sm: '/images/apps/poap.svg',
-			lg: '',
-		},
-		title: 'POAP',
-		subtitle: 'poap.worldcoin.org',
-		description: 'The bookmarks of your life. Issue POAPs to humans only. One person, one POAP.',
-		tags: ['On-chain', 'Integration'],
-	},
-
-	{
-		url: 'https://worldcoin.org',
-		githubUrl: 'https://github.com/worldcoin/world-id-example-airdrop',
-		image: {
-			sm: '/images/apps/hyperdrop.svg',
-			lg: '',
-		},
-		title: 'Worldcoin Airdrop',
-		subtitle: 'worldcoin.org',
-		description:
-			'The first token to be globally and freely distributed to people, just for being a unique individual.',
-		tags: ['On-chain', 'App'],
-		worldcoin: false,
-	},
-
-	{
-		url: 'https://ethglobal.com/showcase/world-id-mina-embt9',
-		githubUrl: 'https://github.com/mitschabaude/mina-world-id',
-		image: {
-			sm: '/images/apps/ethbogota.jpg',
-			lg: '/images/apps/ethbogota.jpg',
-		},
-		title: 'World ID on Mina',
-		subtitle: 'mitschabaude/mina-world-id',
-		description: 'Implementing the World ID system in snarkyjs, making PoP available for Mina smart contracts.',
-		tags: ['Hackathon Project', 'On-chain', 'Integration'],
-		worldcoin: false,
-	},
-
-	{
-		url: 'https://devfolio.co/projects/starksight-5d82',
-		githubUrl: 'https://github.com/HerodotusDev/starksight-zkhack',
-		image: {
-			sm: '/images/apps/zkhack.png',
-			lg: '/images/apps/zkhack.png',
-		},
-		title: 'StarkSight',
-		subtitle: 'HerodotusDev/starksight-zkhack',
-		description: 'Using Herodotus to make World ID verifiable on Starknet.',
-		tags: ['Hackathon Project', 'On-chain', 'Integration'],
-		worldcoin: false,
-	},
-]
-
 // ANCHOR: tags handling
-const tags = apps.reduce((accumulator: Array<string>, example) => {
+const tags = sortedApps.reduce((accumulator: Array<string>, example) => {
 	if (accumulator.length === 0) {
 		return example.tags.sort()
 	}
@@ -363,3 +219,21 @@ const tags = apps.reduce((accumulator: Array<string>, example) => {
 
 	return accumulator.concat(tagsToAdd).sort()
 }, [])
+
+export type AppConfig = {
+	slug: string
+	url: string
+	githubUrl?: string
+	image: {
+		sm: string
+		lg?: string
+	}
+	title: string
+	subtitle: string
+	description: string
+	tags: string[]
+	body?: string
+	bookmark?: boolean
+	worldcoin?: boolean
+	legacy?: boolean
+}

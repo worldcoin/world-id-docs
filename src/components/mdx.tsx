f616db3f4984b72feec45c8e4db2456d0450a767
0xPenryn <penryn@pm.me>
2023-11-27 15:14:50 -0600

    feat: add `required` and `defaultValue` props to Property component

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -69,16 +69,27 @@ export const Property: FC<
 	PropsWithChildren<{
 		name: string
 		type: string
+		defaultValue?: string
+		required?: boolean
 	}>
-> = ({ name, type, children }) => (
+> = ({ name, type, defaultValue, required, children }) => (
 	<li className="m-0 px-0 py-4 first:pt-0 last:pb-0">
 		<dl className="m-0 flex flex-wrap items-center gap-x-3 gap-y-2">
 			<dt className="sr-only">Name</dt>
 			<dd>
 				<code>{name}</code>
 			</dd>
-			<dt className="sr-only">Type</dt>
-			<dd className="font-mono text-xs text-zinc-400 dark:text-zinc-500">{type}</dd>
+			{defaultValue ? <>
+				<dt className="sr-only">Type</dt>
+				<dd className="font-mono text-xs text-zinc-400 dark:text-zinc-500">{type}: <code className='text-zinc-500'>{defaultValue}</code></dd>
+			</> : <>
+				<dt className="sr-only">Type</dt>
+				<dd className="font-mono text-xs text-zinc-400 dark:text-zinc-500">{type}</dd>
+			</>}
+			{required && <>
+				<dt className="sr-only">Required</dt>
+				<dd className=" text-xs text-red-400 dark:text-red-500">REQUIRED</dd>
+			</>}
 			<dt className="sr-only">Description</dt>
 			<dd className="w-full flex-none [&>:first-child]:mt-0 [&>:last-child]:mb-0">{children}</dd>
 		</dl>

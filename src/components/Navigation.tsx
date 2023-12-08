4e8c9a86d212325d38ae465163d153fe17b2542b
0xPenryn <penryn@pm.me>
2023-12-08 17:25:13 -0600

    feat: world id lite page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -201,6 +201,7 @@ export const navigation = [
 			{ title: 'OIDC Explainer', href: '/further-reading/oidc' },
 			{ title: 'Protocol Internals', href: '/further-reading/protocol-internals' },
 			{ title: 'Zero-Knowledge Proofs', href: '/further-reading/zero-knowledge-proofs' },
+			{ title: 'World ID Lite', href: '/further-reading/world-id-lite' },
 			{ title: 'World ID Reset', href: '/further-reading/world-id-reset' },
 		],
 	},

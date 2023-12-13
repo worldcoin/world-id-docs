cf2ee592c22562d9ea57640b2080cd92a4881e28
0xPenryn <penryn@pm.me>
2023-12-13 14:51:10 +0100

    fix: apps link

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/apps',
-				destination: 'https://worldcoin.org/apps',
+				destination: 'https://worldcoin.org/explore-apps',
 				permanent: true,
 			},
 			{

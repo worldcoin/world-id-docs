f21c30b1860e8172a035cf45db6b9f2ac39f5a18
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-06-02 10:39:43 +0200

    animate toggle (#273)

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,12 +1,8 @@
-import clsx from 'clsx'
 import Link from 'next/link'
-import { Children, ReactNode, useMemo } from 'react'
 
 export const ToggleButton = (props: { isActive: boolean; label: string; href: string }) => (
 	<Link
-		className={clsx('font-medium rounded-lg py-2.5 text-sm  text-gray-900 text-center', {
-			'bg-white shadow-toggle': props.isActive,
-		})}
+		className="font-medium text-sm py-2.5 text-gray-900 text-center relative"
 		aria-disabled={props.isActive}
 		href={props.href}
 	>

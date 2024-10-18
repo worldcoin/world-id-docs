684bc88bb9de36ce98d4c751840904328cc2e3c3
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-17 17:16:59 -0700

    fix redirects (#291)
    * fix redirects
    
    * fix

--- next.config.mjs
+++ next.config.mjs
@@ -22,12 +22,6 @@ const nextConfig = {
 	},
 	async redirects() {
 		return [
-            // remove when world chain docs are live 
-			{
-				source: '/world-chain',
-				destination: 'https://worldcoin.notion.site/World-Chain-Developer-Preview-Guide-23c94a67683f4e71986e5303ab88c9f3',
-				permanent: false,
-			},
 			{
 				source: '/apps',
 				destination: 'https://worldcoin.org/apps',
@@ -40,12 +34,12 @@ const nextConfig = {
 			},
 			{
 				source: '/idkit',
-				destination: '/world-id/id/cloud',
+				destination: '/world-id/cloud',
 				permanent: true,
 			},
 			{
 				source: '/js',
-				destination: '/world-id/id/cloud',
+				destination: '/world-id/cloud',
 				permanent: true,
 			},
 			{
@@ -65,7 +59,7 @@ const nextConfig = {
 			},
 			{
 				source: '/test',
-				destination: '/world-id/quick-start/testing',
+				destination: '/quick-start',
 				permanent: false,
 			},
 			{
@@ -80,12 +74,12 @@ const nextConfig = {
 			},
 			{
 				source: '/zkp',
-				destination: '/world-id/further-reading/zero-knowledge-proofs',
+				destination: '/world-id/advanced/zero-knowledge-proofs',
 				permanent: false,
 			},
 			{
 				source: '/id/anonymous-actions',
-				destination: '/world-id/id/incognito-actions',
+				destination: '/world-id/incognito-actions',
 				permanent: false,
 			},
 		]

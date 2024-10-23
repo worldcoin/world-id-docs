ee56044f918a8a6644bb981eff6b234b4b96d010
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-23 17:55:14 -0400

    small docs improvements (#303)
    * small docs improvements
    
    * bump packages
    
    * fix types
    
    * fix

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,6 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	swcMinify: true,
 	reactStrictMode: true,
 	pageExtensions: ['ts', 'tsx', 'mdx'],
 	experimental: {

ba475c24473c1f02bcba72c8f762d3fc2f22333b
Michal Struck <michal.o.struck@gmail.com>
2025-01-13 14:09:08 +0100

    refactor: usernames

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -265,7 +265,7 @@ export const miniAppsNavigation = [
 			{ title: 'Sharing', href: '/mini-apps/reference/sharing' },
 			{ title: 'Errors', href: '/mini-apps/reference/errors' },
 			{ title: 'Address Book', href: '/mini-apps/reference/address-book' },
-			{ title: 'Misc', href: '/mini-apps/reference/misc' },
+			{ title: 'Usernames', href: '/mini-apps/reference/usernames' },
 		],
 	},
 	{

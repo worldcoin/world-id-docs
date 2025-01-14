5650a39efc6d40107ccfe74bdb0b6a51deddb9a4
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2025-01-15 00:20:55 +0100

    mention how to access the profile pic and username (#342)
    * feat: mention how to access the profile pic and username
    
    * refactor: usernames

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -265,6 +265,7 @@ export const miniAppsNavigation = [
 			{ title: 'Sharing', href: '/mini-apps/reference/sharing' },
 			{ title: 'Errors', href: '/mini-apps/reference/errors' },
 			{ title: 'Address Book', href: '/mini-apps/reference/address-book' },
+			{ title: 'Usernames', href: '/mini-apps/reference/usernames' },
 		],
 	},
 	{

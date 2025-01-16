a6a921ee720f35e2db7edce7ec3ea90f4d2f50d5
Mateo Sauton <39073982+mateosauton@users.noreply.github.com>
2025-01-16 15:36:35 -0300

    Quick actions (#348)
    * Quick Actions

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -250,11 +250,14 @@ export const miniAppsNavigation = [
 		title: 'Technical Reference',
 		links: [
 			{ title: 'API Reference', href: '/mini-apps/reference/api' },
-			{ title: 'Sharing', href: '/mini-apps/reference/sharing' },
 			{ title: 'Errors', href: '/mini-apps/reference/errors' },
 			{ title: 'Address Book', href: '/mini-apps/reference/address-book' },
 			{ title: 'Usernames', href: '/mini-apps/reference/usernames' },
 		],
+	},
+	{
+		title: 'Quick Actions',
+		links: [{ title: 'Quick Actions', href: '/mini-apps/sharing/quick-actions' }],
 	},
 	{
 		title: 'Further Reading',

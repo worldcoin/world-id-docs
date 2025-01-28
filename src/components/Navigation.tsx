fbf5b7de6fb713f372a554f52997b2422927a352
Mateo Sauton <39073982+mateosauton@users.noreply.github.com>
2025-01-28 14:53:24 -0300

    Eggs Vault Quick Action Page (#362)
    * Eggs Vault Quick Action Page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -260,6 +260,7 @@ export const miniAppsNavigation = [
 		links: [
 			{ title: 'Quick Actions', href: '/mini-apps/sharing/quick-actions' },
 			{ title: 'UNO Swap', href: '/mini-apps/sharing/uno-qa' },
+			{ title: 'Eggs Vault Smash', href: '/mini-apps/sharing/eggs-vault-qa' },
 		],
 	},
 	{

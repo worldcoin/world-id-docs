16dca395096bdd3395a97e5153666c21005ed9de
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-03-08 12:27:27 -0800

    add site verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
+			<meta name="algolia-site-verification" content="9ED47D91389C7847" />
+
 			<body className="bg-white antialiased dark:bg-zinc-900">
 				<Main />
 				<NextScript />

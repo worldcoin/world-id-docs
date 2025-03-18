3e76d2d60a999fdfe2d679b392d467904e4fba49
Mateo Sauton <mateo.sauton@toolsforhumanity.com>
2025-03-18 13:06:27 -0300

    Orbital X Quick Action

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -275,6 +275,7 @@ export const miniAppsNavigation = [
 			{ title: 'World Companies Finder', href: '/mini-apps/sharing/world-companies-finder-qa' },
 			{ title: 'OnePay', href: '/mini-apps/sharing/one-pay-qa' },
 			{ title: 'PUF', href: '/mini-apps/sharing/puf-qa' },
+			{ title: 'ORBITAL-X', href: '/mini-apps/sharing/orbital-x-qa' },
 		],
 	},
 	{

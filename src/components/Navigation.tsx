bd1378bbfe2938bd97b59c5dd1df85fb9883735b
Mateo Sauton <mateo.sauton@toolsforhumanity.com>
2025-03-18 11:22:33 -0300

    World Companies Finder Quick Action

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -272,6 +272,7 @@ export const miniAppsNavigation = [
 			{ title: 'DNA Send & Swap', href: '/mini-apps/sharing/dna-qa' },
 			{ title: 'Sage Support', href: '/mini-apps/sharing/sage-qa' },
 			{ title: 'World Chat', href: '/mini-apps/sharing/world-chat-qa' },
+			{ title: 'World Companies Finder', href: '/mini-apps/sharing/world-companies-finder-qa' },
 		],
 	},
 	{

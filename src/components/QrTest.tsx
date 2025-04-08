e64d8f3601f00f4e6c61378670439a62ed91c37b
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-08 13:33:58 -0700

    Add docs examples (#418)
    * add docs exampels
    
    * update docs

--- src/components/QrTest.tsx
+++ src/components/QrTest.tsx
@@ -1,30 +1,34 @@
 import React, { useState } from 'react'
 import { QRCodeCanvas } from 'qrcode.react'
 
-export const QRCodeGenerator: React.FC = () => {
-	const [url, setUrl] = useState<string>('')
+export const QRCodeGenerator: React.FC = (props: { appId?: string; hideInput?: boolean }) => {
+	const [appId, setAppId] = useState<string>(props.appId || '')
 	const [isValid, setIsValid] = useState<boolean>(true)
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const value = event.target.value
 		const regex = /^app_[a-f0-9]+$/
-		setUrl(event.target.value)
+		setAppId(event.target.value)
 		setIsValid(regex.test(value))
 	}
 
 	return (
 		<div>
-			<input
-				type="text"
-				placeholder="Enter App Id (eg. app_f88bb2a....)"
-				value={url}
-				onChange={handleInputChange}
-				className="rounded-lg p-2 border-2 w-96"
-			/>
+			{!props.hideInput && (
+				<input
+					type="text"
+					placeholder="Enter App Id (eg. app_f88bb2a....)"
+					value={appId}
+					onChange={handleInputChange}
+					className="rounded-lg p-2 border-2 w-96"
+				/>
+			)}
 			{!isValid && <p style={{ color: 'red' }}>Invalid App Id. Eg. app_xxxxxxxxxxx</p>}
 			<div style={{ marginTop: '20px' }}>
-				{url && isValid && <QRCodeCanvas value={`https://worldcoin.org/mini-app?app_id=${url}`} size={200} />}
-				{!url ||
+				{appId && isValid && (
+					<QRCodeCanvas value={`https://worldcoin.org/mini-app?app_id=${appId}`} size={200} />
+				)}
+				{!appId ||
 					(!isValid && (
 						<div className="h-[200px] w-[200px] bg-gray-100 rounded-lg text-red-700 items-center flex justify-center font-bold"></div>
 					))}

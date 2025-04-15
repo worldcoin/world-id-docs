9bb901a2cbcc640c2dd95414547e247416d7955c
Mateo Sauton <mateo.sauton@toolsforhumanity.com>
2025-04-15 17:47:16 +0900

    developer rewards pilot
    also added alchemy incentives

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -285,6 +285,7 @@ export const miniAppsNavigation = [
 			{ title: 'App Store Policy', href: '/mini-apps/more/policy' },
 			{ title: 'Promotion', href: '/mini-apps/more/promotion' },
 			{ title: 'Grants', href: '/mini-apps/more/grants' },
+			{ title: 'Developer Rewards Pilot', href: '/mini-apps/more/developer-rewards-pilot' },
 			{ title: 'Troubleshooting', href: '/mini-apps/more/troubleshooting' },
 			{ title: 'Webview Specifications', href: '/mini-apps/more/webview-spec' },
 			{ title: 'Releases', href: '/mini-apps/more/releases' },

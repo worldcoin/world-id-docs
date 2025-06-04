7ac410352a29057cca1a50b3ae548865bc628e9c
Mateo Sauton <mateo.sauton@toolsforhumanity.com>
2025-06-04 16:44:55 -0300

    Renamed section

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -306,7 +306,7 @@ export const miniAppsNavigation = [
 			{ title: 'Troubleshooting', href: '/mini-apps/more/troubleshooting' },
 			{ title: 'Webview Specifications', href: '/mini-apps/more/webview-spec' },
 			{ title: 'Releases', href: '/mini-apps/more/releases' },
-			{ title: 'Partners', href: '/mini-apps/more/partners' },
+			{ title: 'Community Tools & Perks', href: '/mini-apps/more/partners' },
 		],
 	},
 ]
